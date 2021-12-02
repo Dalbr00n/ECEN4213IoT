762080964aa4b254f54666f14ad413aa85e8f08c
payton <payton.fack@okstate.edu>
2021-12-02 10:22:37 -0600

    add a bunch of stuff

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const mysql = require('mysql2');
+const bodyParser = require("body-parser");
+const net = require('net');
+const util = require('util');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
 
 //joystick dependences to send and recieve
 var http = require('http');
@@ -11,9 +19,38 @@ var http = require('http');
 
 const port = 3000;
 
-var xpos;
-var ypos;
+//used to prevent overflow of database values
+const start = Date.now();
 
+//initialize mysqlconnection
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'node',
+  password: '',
+  database: 'KO',
+  multipleStatements: true
+});
+connection.connect(function(err){
+  if(err){
+    console.error('error: ' + err.message);
+  }
+
+  console.log('MySQL connection successful!');
+});
+
+//reset the tables
+const sqlinit = 'DROP TABLE IF EXISTS KODATA;'+
+                'CREATE TABLE KODATA('+
+                'time_in_ms int primary key,L_bump varchar(1),C_bump varchar(1),R_bump varchar(1),L_cliff varchar(1), C_cliff varchar(1), R_cliff varchar(1), L_wheel varchar(1), R_wheel varchar(1));';
+connection.query(sqlinit, (error,results,feilds) =>{
+  if(error){
+    console.log("error: " + error.message);
+  }
+
+  console.log("Database Initialized")
+});
+
+/*
 //code to deal with the server joy commands
 http.createServer(function (request,res) {
   if (request.method == 'POST') {
@@ -35,24 +72,92 @@ http.createServer(function (request,res) {
           res.end();
       });
   }
-}).listen(3001);
+  //getting data from the sql server
+  if(request.method == 'GET'){
+
+  }
+}).listen(3001);*/
 // listening on port 3001 for the POST request
 
-app.use(express.static("public"));
+
+
+app.use(express.static(__dirname + "/public"));
 app.use("/img", express.static("img"));
+app.use("/public",express.static("public"));
 
 //opens up the index file for the server
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-//emits the camera image (data) to the webpage as 'web'
-setInterval(function(){
-    io.emit('web',data);
-}, 500);
+//handles the joystick data
+app.post('/joydata',function(req,res){
+  var xpos = req.body.x;
+  var ypos = req.body.y;
 
+  console.log("xpos: " + xpos + " ypos: " + ypos + "\n");
+  //this makes c++ wayyyy easier
+  
+  res.end();
+});
+
+//handles getting sensor information
+app.post('/sensordata',function(req,res){
+
+  console.log(req.body.t);
+
+  //building the SQL query
+
+  //time in ms to get up to based on user request
+  const upTo = (Date.now()-start) - req.body.t;
+
+  const query = "SELECT * FROM KODATA WHERE time_in_ms > "+ upTo + " ORDER BY time_in_ms DESC";
+
+  connection.query(query,(error,results,feilds) =>{
+    console.log(results);
+    res.send(results);
+  });
+});
 
 //open up the server on port: port (defined above)
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
+const cComm = net.createServer((c) =>{
+  console.log('client connected');
+
+  c.on('end',()=>{
+    console.log('client disconncected');
+  });
+
+  c.on('data', (data) => {
+    //decode the array buffer
+    var dec = new util.TextDecoder("utf-8");
+
+    io.emit('web', dec.decode(data));
+  });
+});
+
+
+
+cComm.listen(3004, (data) =>{
+  console.log(data);
+});
+
+
+
+
+
+
+
+//test code to fill the server with dummy data
+setInterval(function(){
+  const query = "INSERT INTO KODATA (time_in_ms, L_bump,R_cliff,R_wheel) VALUES (" + (Date.now()-start) + ",'1','0','1')";
+
+  connection.query(query, (error, results, feilds) =>{
+    if(error){
+      console.log("error: "+error.message);
+    }
+  });
+},1000);
